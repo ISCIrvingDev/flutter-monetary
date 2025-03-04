f65074ce221b07158a678a081ac81641c9596293
Irving Salazar <ISCIrvingDev@gmail.com>
2025-03-04 17:22:53 -0600

    Add money format to "balence" value

--- lib/components/bank_balance.component.dart
+++ lib/components/bank_balance.component.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 List<Widget> bankBalance(double balance) {
-  return [Text('Bank Balance'), SizedBox(height: 20), Text(('$balance'))];
+  return [
+    Text('Bank Balance'),
+    SizedBox(height: 20),
+    Text(
+      '\$ ${NumberFormat.simpleCurrency(name: '').format(balance)}',
+      style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+    ),
+  ];
 }
